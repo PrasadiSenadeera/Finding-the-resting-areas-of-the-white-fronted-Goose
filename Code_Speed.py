926528038b3ef7a89320ee02287ed1511b8fecd9
Jannis Fröhlking <38653702+JaFro96@users.noreply.github.com>
2020-07-10 18:04:34 +0200

    Improved coding style
    use more function, use of docstrings, simplified output path

--- Code_Speed.py
+++ Code_Speed.py
@@ -1,4 +1,8 @@
-import os, ogr, datetime, math, time
+import os
+import ogr
+import datetime
+import math
+import time
 from qgis.core import *
 from qgis.gui import *
 import qgis.utils
@@ -6,225 +10,218 @@ import numpy as np
 from matplotlib import pyplot as plt
 import statistics
 
-
-# load the point shapefile manually!
-
-# access the active layer
-point_layer = iface.activeLayer()
-if not point_layer:
-    print("Shape file failed to load!")
-else: print("Shape file loaded!")
-
-#Add new colums
-
+# This code computes and plots the speed of the goose per datapoint
+# After that it splits the data based on a threshold into resting and flying points
 def newColumn (layer,FieldName,DataType):
-    caps = layer.dataProvider().capabilities()
-    if caps & QgsVectorDataProvider.AddAttributes:
-        res = point_layer.dataProvider().addAttributes([QgsField(FieldName,DataType)])
-# Update to propagate the changes
-    point_layer.updateFields()
-
-
-# Get the index of the new field
-def getIndex(layer,Field_name):
-    index=0
-    fields = layer.dataProvider().fields()
-    for field in layer.fields():
-        if field.name() == Field_name:
-            break
-        index += 1
-    return index
-
-# Check if field already exists
-if  point_layer.fields().indexFromName("Ob_Time") == -1:
-    #Create field for store Date
-    newColumn (point_layer,"Ob_Time", QVariant.String)
-
-# Check if field already exists
-if point_layer.fields().indexFromName("Ob_Date") == -1:
-    #Create field for store Date
-    newColumn (point_layer,"Ob_Date", QVariant.String)
-
-# Initiate a variable to hold the date and time values extracted from shape file
-updates_time = {}
-updates_date={}
-for feat in point_layer.getFeatures():
-    # Get the date time value from the gpx
-    date_time = feat['timestamp']
-    date_time_obj = datetime.datetime.strptime(date_time,'%Y-%m-%d %H:%M:%S')
-    time = date_time_obj.strftime("%H:%M:%S")
-    date = date_time_obj.strftime("%Y-%m-%d")
-
-    # Update the empty fields in the shapefile
-    indexT=getIndex(point_layer,'Ob_Time')
-    indexD=getIndex(point_layer,'Ob_Date')
-    updates_time[feat.id()] = {indexT:time}
-    updates_date[feat.id()] = {indexD:date}
-#print(updates)
-
-# Use the created dictionary to update the field for all features
-point_layer.dataProvider().changeAttributeValues(updates_time)
-point_layer.dataProvider().changeAttributeValues(updates_date)
-# Update to propagate the changes
-point_layer.updateFields()
-
-########################################Calculate the distance between two points###########################
-#Store route identification codes in to a list
-L_tracks=['"tag_ident"=72413','"tag_ident"=72417','"tag_ident"=73053','"tag_ident"=72364',\
-'"tag_ident"=73054','"tag_ident"=79694','"tag_ident"=79698']
-
-
-# Check if field already exists
-if point_layer.fields().indexFromName("Distance") == -1:
-    #Create field for store Distance
-    newColumn (point_layer,"Distance", QVariant.Double)
-
-#create empty lists to save UTM coordinates, track number, feature ID
-
-#Select seperate locations(points) of each route and save their properties in to lists
-for m in range(0,len(L_tracks)):
-    L_north=[]
-    L_east=[]
-    L_ID=[]
-    L_distance=[]
-
-    point_layer.selectByExpression(L_tracks[m], QgsVectorLayer.SetSelection)
-    selection = point_layer.selectedFeatures()
-    for feature in selection:
-        east=feature['utm_east']
-        north=feature['utm_north']
-        L_north.append(north)
-        L_east.append(east)
-        L_ID.append(feature.id())
-
-    #Calculate the euclidean distance between a point and its previous point
-    for j in range (0,(len(L_north))):
-        if j==0:
-            distance=0
-            L_distance.append(distance)
-        else:
-            D_north=(L_north[j]-L_north[j-1])**2
-            D_East=(L_east[j]-L_east[j-1])**2
-            distance=math.sqrt(D_north+D_East)
-            L_distance.append(distance)
-
-    # for check the values
-    #print(L_north[0],L_east[0],L_ID[0],L_distance[0])
-    #print(L_north[1],L_east[1],L_ID[1],L_distance[1])
-    #print(L_north[2],L_east[2],L_ID[2],L_distance[2])
-
-    #Update distances to a new field
-
-    updates_distance={}
-    for i in range (0,(len(L_north))):
-        # Get the distance value from the gpx
-        distance=L_distance[i]
-        index=L_ID[i]
-
-        # Update the empty fields in the shapefile
-        indexDi=getIndex(point_layer,'Distance')
-        updates_distance[index] = {indexDi:distance}
-
-    point_layer.dataProvider().changeAttributeValues(updates_distance)
-    # Update to propagate the changes
-    point_layer.updateFields()
-    point_layer.removeSelection()
-
-    L_north.clear()
-    L_east.clear()
-    L_ID.clear()
-    L_distance.clear()
-
-###########################################Calculate time difference########################################
-
-# Check if field already exists
-if point_layer.fields().indexFromName("TimeDiff") == -1:
-    #Create field to store TimeDifference
-    newColumn (point_layer,"TimeDiff", QVariant.Double)
-
-#Create a list to store time values
-for m in range(0,len(L_tracks)):
-    L_Datetime=[]
-    L_ID=[]
-    L_TimeDiff=[]
-
-    point_layer.selectByExpression(L_tracks[m], QgsVectorLayer.SetSelection)
-    selection = point_layer.selectedFeatures()
-    for feature in selection:
-        Datetime=feature['timestamp']
-        L_Datetime.append(Datetime)
-        L_ID.append(feature.id())
-
-
-    #Calculate time between a point and its previous point
-    for j in range (0,(len(L_ID))):
-        if j==0:
-            TimeDiff=0
-            L_TimeDiff.append(TimeDiff)
-        else:
-            To_time=datetime.datetime.strptime(L_Datetime[j],'%Y-%m-%d %H:%M:%S')
-            From_time=datetime.datetime.strptime(L_Datetime[j-1],'%Y-%m-%d %H:%M:%S')
-            TimeDiff=To_time-From_time
-
-            value=TimeDiff.total_seconds()
-            L_TimeDiff.append(value)
-
-    # for check the values
-
-    #print(L_Datetime[0],L_TimeDiff[0])
-    #print(L_Datetime[1],L_TimeDiff[1])
-    #print(L_Datetime[2],L_TimeDiff[2])
-
-
-    #Update time difference to a new field
-
-    updates_timeDiff={}
-    for i in range (0,(len(L_TimeDiff))):
-        # Get the distance value from the gpx
-        TimeDiff=L_TimeDiff[i]
-        index=L_ID[i]
-
-        # Update the empty fields in the shapefile
-        indexTimeDiff=getIndex(point_layer,'TimeDiff')
-        updates_timeDiff[index] = {indexTimeDiff:TimeDiff}
-
-    point_layer.dataProvider().changeAttributeValues(updates_timeDiff)
-    # Update to propagate the changes
-    point_layer.updateFields()
-    point_layer.removeSelection()
-
-    L_Datetime.clear()
-    L_ID.clear()
-    L_TimeDiff.clear()
-
-##############################################Calculate speed##################################################
-
-# Check if field already exists
-if point_layer.fields().indexFromName("Speed") == -1:
-    #Create new field to store speed
-    newColumn (point_layer,"Speed", QVariant.Double)
-
-updates_speed={}
-for feat in point_layer.getFeatures():
-    a=feat['Distance']
-    b=feat['TimeDiff']
-
-    if (a==0 or b==0) :
-        speed=0
+    """
+    Adds a new field to the layer.
+        Parameters:
+            layer: QGIS layer object
+            FieldName(String): Name of the new fields
+            Datatype: QVariant DataType
+    """
+    # Check if field already exists
+    if layer.fields().indexFromName(FieldName)==-1:
+        caps = layer.dataProvider().capabilities()
+        if caps & QgsVectorDataProvider.AddAttributes:
+            res = layer.dataProvider().addAttributes([QgsField(FieldName,DataType)])
+            print("New field \"{}\" added".format(FieldName))
+        # Update to propagate the changes
+        layer.updateFields()
     else:
-        speed=a/b/1000*3600
-    index=feat.id()
+        print("Field \"{}\" already exists.".format(FieldName))
 
-    indexSpeed=getIndex(point_layer,'Speed')
-    updates_speed[index] = {indexSpeed:speed}
 
-point_layer.dataProvider().changeAttributeValues(updates_speed)
-point_layer.updateFields()
-point_layer.removeSelection()
+def addTimeAndDateObs(layer):
+    """
+    Initiate a variable to hold the date and time values extracted from shape file and populate them.
+        Parameters:
+            layer: QGIS layer object
+    """
+    #Create field for storing time of observation
+    newColumn (layer,"Ob_Time", QVariant.String)
+    #Create field for storing date of observation
+    newColumn (layer,"Ob_Date", QVariant.String)
+    # Empty objects for storing the updates
+    updates_time = {}
+    updates_date = {}
+    indexT=layer.fields().indexFromName('Ob_Time')
+    indexD=layer.fields().indexFromName('Ob_Date')
+    print("STARTING LOOP!")
+    for feat in layer.getFeatures():
+        # Get the date time value from the gpx
+        date_time = feat['timestamp']
+        date_time_obj = datetime.datetime.strptime(date_time,'%Y-%m-%d %H:%M:%S')
+        time = date_time_obj.strftime("%H:%M:%S")
+        date = date_time_obj.strftime("%Y-%m-%d")
+        # Update the empty fields in the shapefile
+        updates_time[feat.id()] = {indexT:time}
+        updates_date[feat.id()] = {indexD:date}
+    #print(updates)
 
-# Function for calculating basic statistics of the speed field
-# @return Median, because it splits the data equally into two groups
+    # Use the created dictionary to update the field for all features
+    layer.dataProvider().changeAttributeValues(updates_time)
+    layer.dataProvider().changeAttributeValues(updates_date)
+    # Update to propagate the changes
+    layer.updateFields()
+    print("Time and date fields populated.")
+
+def addDistance(layer, tracks):
+    """
+    Calculate the distance between two points and add a field for that purpose.
+        Parameters:
+            layer: QGIS layer object
+            tracks: List containing trackID of different geese
+    """
+
+    #Create field for store Distance
+    newColumn (layer,"Distance", QVariant.Double)
+
+    #Select seperate locations(points) of each route and save their properties in to lists
+    for m in range(0,len(tracks)):
+        #create empty lists to save UTM coordinates, track number, feature ID
+        L_north=[]
+        L_east=[]
+        L_ID=[]
+        L_distance=[]
+
+        layer.selectByExpression(tracks[m], QgsVectorLayer.SetSelection)
+        selection = layer.selectedFeatures()
+        for feature in selection:
+            east=feature['utm_east']
+            north=feature['utm_north']
+            L_north.append(north)
+            L_east.append(east)
+            L_ID.append(feature.id())
+
+        #Calculate the euclidean distance between a point and its previous point
+        for j in range (0,(len(L_north))):
+            if j==0:
+                distance=0
+                L_distance.append(distance)
+            else:
+                D_north=(L_north[j]-L_north[j-1])**2
+                D_East=(L_east[j]-L_east[j-1])**2
+                distance=math.sqrt(D_north+D_East)
+                L_distance.append(distance)
+
+        #Update distances to a new field
+        updates_distance={}
+        for i in range (0,(len(L_north))):
+            # Get the distance value from the gpx
+            distance=L_distance[i]
+            index=L_ID[i]
+
+            # Update the empty fields in the shapefile
+            indexDi=layer.fields().indexFromName('Distance')
+            updates_distance[index] = {indexDi:distance}
+
+        layer.dataProvider().changeAttributeValues(updates_distance)
+        # Update to propagate the changes
+        layer.updateFields()
+        layer.removeSelection()
+
+        L_north.clear()
+        L_east.clear()
+        L_ID.clear()
+        L_distance.clear()
+
+def calcTimeDiff(layer, tracks):
+    """
+    Calculate the time difference between two points and add a field for that purpose.
+        Parameters:
+            layer: QGIS layer object
+            tracks: List containing trackID of different geese
+    """
+
+    # Check if field already exists
+    if layer.fields().indexFromName("TimeDiff") == -1:
+        #Create field to store TimeDifference
+        newColumn (layer,"TimeDiff", QVariant.Double)
+
+    #Create a list to store time values
+    for m in range(0,len(tracks)):
+        L_Datetime=[]
+        L_ID=[]
+        L_TimeDiff=[]
+
+        layer.selectByExpression(tracks[m], QgsVectorLayer.SetSelection)
+        selection = layer.selectedFeatures()
+        for feature in selection:
+            Datetime=feature['timestamp']
+            L_Datetime.append(Datetime)
+            L_ID.append(feature.id())
+
+
+        #Calculate time between a point and its previous point
+        for j in range (0,(len(L_ID))):
+            if j==0:
+                TimeDiff=0
+                L_TimeDiff.append(TimeDiff)
+            else:
+                To_time=datetime.datetime.strptime(L_Datetime[j],'%Y-%m-%d %H:%M:%S')
+                From_time=datetime.datetime.strptime(L_Datetime[j-1],'%Y-%m-%d %H:%M:%S')
+                TimeDiff=To_time-From_time
+
+                value=TimeDiff.total_seconds()
+                L_TimeDiff.append(value)
+        #Update time difference to a new field
+
+        updates_timeDiff={}
+        for i in range (0,(len(L_TimeDiff))):
+            # Get the distance value from the gpx
+            TimeDiff=L_TimeDiff[i]
+            index=L_ID[i]
+
+            # Update the empty fields in the shapefile
+            indexTimeDiff=layer.fields().indexFromName('TimeDiff')
+            updates_timeDiff[index] = {indexTimeDiff:TimeDiff}
+
+        layer.dataProvider().changeAttributeValues(updates_timeDiff)
+        # Update to propagate the changes
+        layer.updateFields()
+        layer.removeSelection()
+
+        L_Datetime.clear()
+        L_ID.clear()
+        L_TimeDiff.clear()
+
+def calcSpeed(layer):
+    """
+    Calculate the speed between two points and store it into a new field.
+        Parameters:
+            layer: QGIS layer object
+    """
+    #Create new field to store speed
+    newColumn (layer,"Speed", QVariant.Double)
+
+    updates_speed={}
+    for feat in layer.getFeatures():
+        a=feat['Distance']
+        b=feat['TimeDiff']
+
+        if (a==0 or b==0) :
+            speed=0
+        else:
+            speed=a/b/1000*3600
+        index=feat.id()
+
+        indexSpeed=layer.fields().indexFromName('Speed')
+        updates_speed[index] = {indexSpeed:speed}
+
+    layer.dataProvider().changeAttributeValues(updates_speed)
+    layer.updateFields()
+    layer.removeSelection()
+
 def descriptiveStatisticsSpeed(layer):
+    """
+    Function for calculating basic statistics of the speed field.
+        Parameters:
+            layer: QGIS layer object
+        Returns:
+            Median
+    """
+
     features = layer.getFeatures()
     list_speed = []
     # Iterate over features and add to a list
@@ -252,9 +249,13 @@ def descriptiveStatisticsSpeed(layer):
     plt.show()
     return median_speed
 
-# Function for selecting points with speed value below a specified Threshold
-# Creates new shapefile for selected points.
-def extractSlowPoints(layer, threshold):
+def extractSlowPoints(layer, threshold,outfn):
+    """
+    Function for selecting points with speed value below a specified Threshold. Creates new shapefile for selected points.
+        Parameters:
+            layer: QGIS layer object
+            threshold (double): Speed value determining the split
+    """
     # Feature selection using a threshold for speed
     layer.selectByExpression( "\"Speed\"< {}".format(threshold))
     print("Selection based on this value: {} km/h".format(threshold))
@@ -262,15 +263,45 @@ def extractSlowPoints(layer, threshold):
     # mark selection red in map
     iface.mapCanvas().setSelectionColor( QColor("red") )
     # specify filename
-    dir = os.path.join(os.getcwd())
-    if(os.path.isdir(dir)):
-        fn = os.path.join(dir,'lowSpeed.shp')
-        writer = QgsVectorFileWriter.writeAsVectorFormat(layer, fn, 'utf-8', driverName='ESRI Shapefile', onlySelected=True)
-        selected_layer = iface.addVectorLayer(fn, '', 'ogr')
-        del(writer)
+    fn = os.path.join(outfn,'lowSpeed.shp')
+    writer = QgsVectorFileWriter.writeAsVectorFormat(layer, fn, 'utf-8', driverName='ESRI Shapefile', onlySelected=True)
+    selected_layer = iface.addVectorLayer(fn, '', 'ogr')
+    del(writer)
+
+def main():
+    """
+    Main function calling the other functions.
+    """
+    # IMPORTANT: Specify a path to the new shapefile!
+    out_path = os.path.join("C:\\","Users","janni","OneDrive","Desktop","data")
+
+    #Store route identification codes in to a list
+    L_tracks=['"tag_ident"=72413','"tag_ident"=72417','"tag_ident"=73053','"tag_ident"=72364',\
+    '"tag_ident"=73054','"tag_ident"=79694','"tag_ident"=79698']
+    if(os.path.isdir(out_path)):
+        print("Very good! You have chosen a valid directory!")
+        # load the point shapefile of the white-fronted goose manually!
+        # access the active layer
+        point_layer = iface.activeLayer()
+        if not point_layer:
+            print("Shape file failed to load!")
+        else:
+            # 1
+            addTimeAndDateObs(point_layer)
+            print("-----------1-------------finished!")
+            # 2
+            addDistance(point_layer, L_tracks)
+            print("-----------2-------------finished!")
+            # 3
+            calcTimeDiff(point_layer, L_tracks)
+            print("-----------3-------------finished!")
+            # 4
+            calcSpeed(point_layer)
+            print("-----------4-------------finished!")
+            # 5
+            extractSlowPoints(point_layer,descriptiveStatisticsSpeed(point_layer),out_path)
+            print("-----------5-------------finished!")
+            print('Done')
     else:
-        print("No shapefile created: Please specify a correct directory!")
-
-extractSlowPoints(point_layer,descriptiveStatisticsSpeed(point_layer))
-
-print('Done')
+        print("Please specify a valid directory in the main function of Code_Speed.py!")
+main()
